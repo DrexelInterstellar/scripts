8bb62d3e6257d0aed9f075d28beb7967cd3f3d79
Michael Rinehart <mrinehart97@gmail.com>
2017-05-18 12:43:00 -0400

    User no longer has to look inside code to change variables.
    1. Added OS-agnostic ability to get the current username for working directory.
    2. Removed even more default parameters, CLI asks user to input them instead (TODO: REPLACE WITH GUI INTERFACE)
    3. Threw more stuff in a run() command for slight extensibility

--- GMAT_ITERATOR_v1.0.py
+++ GMAT_ITERATOR_v1.0.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 '''
-    GMAT_ITERATOR_v1.0
+    GMAT_ITERATOR_v1.1
     Author: Michael Rinehart
-    Date: 26 April 2017
+    Date last revised : 18 May 2017
     Platform Created: Windows 7 SP1
     
     Purpose: Reads a GMAT script from the same directory, and
     iteratively runs GMAT using temp scripts with modified
     variables.
 
-    NOTE: FILE PATH CANNOT INCLUDE A SPACE!!
+    NOTE: For now, filepath cannot include a space
     
     Effects: Outputs summary data in a .csv file.
 
@@ -21,48 +21,35 @@ import os
 import platform
 
 
-filenameparam = "GMAT MISC_DATA_PLEASE_REPLACE"
-thrustparam = "GMAT TwinIonEngineThrust"
+def run():
+    #OS-aware code to get path of GMAT and relevant files required to run GMAT.
+    user_platform = platform.system()
+    cwd = os.getcwd()
+    userhome = os.path.expanduser('~')
+    CURRENT_USER = os.path.split(userhome)[-1]
+    if (user_platform == "Windows"):
+        GMAT_LOCATION = "C:\\Users\\" + CURRENT_USER + "\\AppData\\Local\GMAT\\R2016a\\bin\\GMAT.exe"
+        TEMP_SCRIPT_FILE_NAME = cwd + "\\temp.script"
+        SCRIPT_FILE_NAME = cwd + "\\"
+    else: #NOTE: GMAT is required to be in the user's PATH if not on Windows
+        GMAT_LOCATION = "gmat"
+        TEMP_SCRIPT_FILE_NAME = cwd + "/temp.script"
+        SCRIPT_FILE_NAME = cwd
+    
+    SCRIPT_FILE_NAME += input("Please Enter a File Name: ")
+    filenameparam = input("Please enter the name of the file parameter you want to modify: ")
+    thrustparam = input("Please enter the thrust parameter you want to modify: ")
+    start_variable = float(input("Please enter start value: "))
+    end_variable = float(input("Please enter end value: "))
+    step = float(input("Please enter step value: "))
+    
 
-#Mission related constants
-isp = 2100
-start_fuel = 12
-dry_mass = 12
-
-#Actual iteration tools
-start_thrust = 0.001 
-end_thrust = 0.006
-step = 0.0002
-
-start_variable = start_thrust
-end_variable = end_thrust
-
-#OS-aware code to get path of GMAT and relevant files required to run GMAT.
-user_platform = platform.system()
-cwd = os.getcwd()
-if (user_platform == "Windows"):
-    CURRENT_USER = "Michael"
-    GMAT_LOCATION = "C:\\Users\\" + CURRENT_USER + "\\AppData\\Local\GMAT\\R2016a\\bin\\GMAT.exe"
-    TEMP_SCRIPT_FILE_NAME = cwd + "\\temp.script"
-    SCRIPT_FILE_NAME = cwd
-else: #NOTE: GMAT is required to be in the user's PATH if not on Windows
-    GMAT_LOCATION = "gmat"
-    TEMP_SCRIPT_FILE_NAME = cwd + "/temp.script"
-    SCRIPT_FILE_NAME = cwd
-
-if (len(sys.argv) < 2):
-    print("Error: You need to supply a filename!")
-else:
-    SCRIPT_FILE_NAME = cwd + sys.argv[1]
-
-#GMAT data goes in subfolder escape_data, should folder not exist we'll create it
-OUTPUT_FILE = cwd + "\\escape_data\\"
-if not os.path.isdir(OUTPUT_FILE):
-   os.makedirs(OUTPUT_FILE)
-OUTPUT_FILE = OUTPUT_FILE + "escape_data"
-
-def main():
-    print("GMAT Runner Script v p1.0")
+    #GMAT data goes in subfolder escape_data, should folder not exist we'll create it
+    OUTPUT_FILE = cwd + "\\escape_data\\"
+    if not os.path.isdir(OUTPUT_FILE):
+       os.makedirs(OUTPUT_FILE)
+    OUTPUT_FILE = OUTPUT_FILE + "escape_data"
+    print("GMAT Runner Script v 1.1")
     filecounter = 0
     
     template_script_io = open(SCRIPT_FILE_NAME)
@@ -91,6 +78,10 @@ def main():
             data = line.split()
             output_summary_io.write(','.join(map(str, data)))
             output_summary_io.write("\n")
+    
+def main():
+    run()
+    
 
 #Looks in string <content> for <param>, and updates it's value to <param_value>
 #Returns modified string
@@ -106,11 +97,10 @@ def UpdateParameters(content, param, param_value):
 
 #Runs GMAT with the script location and the iteration name
 #Prints status to terminal.
-def runGMAT(file_name, exe_location, iteration):
+def runGMAT(file_name, GMAT, iteration):
     print("Running GMAT with iteration #" + str(iteration) + file_name)
-    subprocess.call(GMAT_LOCATION + " --run --exit --minimize " + file_name)
+    subprocess.call(GMAT + " --run --exit --minimize " + file_name)
 
-#Automatically start data calculation on program run
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
 
